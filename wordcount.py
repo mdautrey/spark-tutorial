771eaac3261d619f52a15d644eb7b391ce4c7ff6
mikael.dautrey <mikael.dautrey@isitix.com>
2015-09-26 10:17:32 +0200

    debug

--- wordcount.py
+++ wordcount.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         exit(-1)
     sc = SparkContext(appName="PythonWordCount")
     lines = sc.textFile(sys.argv[1], 1)
-    counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: x,1).reduceBykey(add)
+    counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: x,1).reduceByKey(lambda x,y:x+y)
     counts.saveAsTextFile(sys.argv[2])
     sc.stop()
