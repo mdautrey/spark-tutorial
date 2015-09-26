97d411d51fd9905b33d4165077e8339f83a44f0a
mikael.dautrey <mikael.dautrey@isitix.com>
2015-09-26 10:46:27 +0200

    stop words

--- wordcount.py
+++ wordcount.py
@@ -7,7 +7,12 @@ if __name__ == "__main__":
         print("Usage: wordcount <input> <output>")
         exit(-1)
     sc = SparkContext(appName="PythonWordCount")
+    common_words = ["the", "a", "an", "and", "of", "to", "in", "am", "is", "are", "at", "not"]
     lines = sc.textFile(sys.argv[1], 1)
-    counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: (x,1)).reduceByKey(lambda x,y:x+y)
+    counts = lines.flatMap(lambda x: re.split(r"[ \t,;\.\?!:@\[\]\(\){}_\*/]+",x))\
+        .filter(lambda x: x.lower() not in common_words and
+                          len(x) > 0)\
+        .map(lambda x: (x, 1))\
+        .reduceByKey(add)
     counts.saveAsTextFile(sys.argv[2])
     sc.stop()
