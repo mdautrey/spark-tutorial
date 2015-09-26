ce3892adb7194ed39f9ee28107610b1adce7c127
mikael.dautrey <mikael.dautrey@isitix.com>
2015-09-26 09:53:34 +0200

    Premiere version

---
+++ wordcount.py
@@ -0,0 +1,13 @@
+import sys
+
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: wordcount <input> <output>")
+        exit(-1)
+    sc = SparkContext(appName="PythonWordCount")
+    lines = sc.textFile(sys.argv[1], 1)
+    counts = lines.flatMap(lambda x: x.split(' ')).map(lambda x: x,1).reduceBBykey(add)
+    counts.saveAsTextFile(sys.argv[2])
+    sc.stop()
